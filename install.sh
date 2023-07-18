28e1ad4850c6114ba3c791619121b39438e11c1e
GenesisHacks <109468312+GenesisHacks@users.noreply.github.com>
2023-07-18 17:31:10 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+USER="$1"
+BACKGROUND="$2"
+LOCKSCREEN="$3"
+#BACKGROUND="https://filebin.net/kndcvsje2oss6kqu/05-upscaled-scale-2_00x__1_.png"
+#LOCKSCREEN="https://filebin.net/kndcvsje2oss6kqu/turtle.png"
+
+function setBackground(
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var allDesktops = desktops();
+    print (allDesktops);
+    for (i=0;i<allDesktops.length;i++) {{
+        d = allDesktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                     "org.kde.image",
+                                     "General");
+        d.writeConfig("Image", "file:///home/'"$USER"'/backlock/$(basename "$BACKGROUND")")
+    }}
+'
+)
+
+
+sudo apt install kde-plasma-desktop
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo 
+flatpak install flathub com.brave.Browser
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.github.iwalton3.jellyfin-media-player
+flatpak install flathub com.nordpass.NordPass
+flatpak install flathub com.github.debauchee.barrier
+cd ~$USER/
+mkdir backlock
+cd backlock
+wget $LOCKSCREEN
+wget $BACKGROUND
+setBackground
+usermod -aG sudo $USER
+shutdown -r 0
